3444c413845db32f29ddff8a134455dcbd0f6b6c
Luke Hofstetter <ldhofstetter@gmail.com>
2023-11-03 14:02:34 -0700

    changed to tab navigation and added tab icons for upload and home

--- App.js
+++ App.js
@@ -3,10 +3,16 @@ import UploadItem from "./components/Upload.js";
 import Explore from "./components/Explore.js";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Button, View, Text, Platform, Pressable } from "react-native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Button, View, Text, Platform, Pressable, Image } from "react-native";
 import { SafeAreaView } from "react-native";
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import PreviewPost from "./components/Preview.js";
+
+const homeFocused = require("./assets/home.png");
+const homeUnfocused = require("./assets/home_unfocused.png");
+const uploadFocused = require("./assets/upload.png");
+const uploadUnfocused = require("./assets/upload_unfocused.png");
 
 const styles = {
   buttonMobile: {
@@ -23,17 +29,9 @@ function Swipe({ navigation }) {
   return Platform.OS == "ios" ? (
     <SafeAreaView>
       <Explore />
-      <Pressable onPressIn={() => navigation.navigate("Upload")} style={styles.buttonMobile}>
-        <Text style={styles.buttonText}>Upload</Text>
-      </Pressable>
     </SafeAreaView>
   ) : (
-    <>
       <Explore />
-      <Pressable onPressIn={() => navigation.navigate("Upload")} style={{marginTop:20}}>
-        <Text style={styles.buttonText}>Upload</Text>
-      </Pressable>
-    </>
   );
 }
 
@@ -61,17 +59,50 @@ function Preview() {
   );
 }
 
-const Stack = createNativeStackNavigator();
+const UploadStack = createNativeStackNavigator();
+
+function UploadRoute () {
+  return (
+    <UploadStack.Navigator>
+        <UploadStack.Screen name="Upload" component={Upload}/>
+        <UploadStack.Screen name="New Listing" component={Preview}/>
+    </UploadStack.Navigator>
+  );
+}
+
+
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <ActionSheetProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Swipe" screenOptions={{ presentation: "modal" }}>
-          <Stack.Screen name="Swipe" component={Swipe} options={{ headerShown: false }} />
-          <Stack.Screen name="Upload" component={Upload} />
-          <Stack.Screen name="New Listing" component={Preview} />
-        </Stack.Navigator>
+        <Tab.Navigator initialRouteName="Swipe" screenOptions={({ route }) => ({
+          presentation: "modal",
+          tabBarIcon: ({ focused }) => {
+            let iconName;
+            let image;
+
+            if (route.name === 'Swipe') {
+              iconName = focused ? 'homeFocus' : 'homeUnfocused';
+              image = focused ? homeFocused : homeUnfocused;
+              
+              return (Platform.OS == 'web' ? <img src={image}/> : <Image source={image}/>);
+            } else if (route.name === 'UploadRoute') {
+              iconName = focused ? 'uploadFocus' : 'uploadUnfocused';
+              image = focused ? uploadFocused : uploadUnfocused;
+
+              return (Platform.OS == 'web' ? <img src={image}/> : <Image source={image}/>);
+            }
+          }, 
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'gray',
+          tabBarShowLabel: false,
+        })}>
+          <Tab.Screen name="Swipe" component={Swipe} options={{ headerShown: false }} />
+          <Tab.Screen name="UploadRoute" component={UploadRoute} options={{ headerShown: false }} />
+        </Tab.Navigator>
       </NavigationContainer>
     </ActionSheetProvider>
   );
