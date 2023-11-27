ea1c91faba40f7855c5378f8034a1d0570ce77c7
Peter Adranly <106859213+PeterAdr14@users.noreply.github.com>
2023-11-26 22:03:22 -0800

    Update match.ts
    Adding an endpoint to get recommendations

--- cinder-server/routers/match.ts
+++ cinder-server/routers/match.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { db } from "../db/index.js";
-import { likes, listings } from "../db/schema.js";
+import { likes, listings, tags } from "../db/schema.js";
 import { auth } from "../lucia.js";
 import { validateListingId } from "../utils/listingValidation.js";
-import { and, eq } from "drizzle-orm";
+import { and, eq, inArray } from "drizzle-orm";
 
 export const matchHandler = express.Router();
+export const recHandler = express.Router();
 
 matchHandler.post("/like/:listing_id", validateListingId, async (req, res) => {
   try {
@@ -58,9 +59,43 @@ matchHandler.post("/dislike/:listing_id", validateListingId, async (req, res) =>
   }
 });
 
+recHandler.get("/recomend/:listing_id", validateListingId, async (req, res) => {
+    try {
+        let { user_id: user_id_string } = req.params;
+        const authRequest = auth.handleRequest(req, res);
+        const session = await authRequest.validate();
+        if (session) { //Check if session is active
+            const tags = await getTags(user_id_string)
+            if (tags.length == 0) { //Check if no tags were retrieved
+                return res.json({ message: "No tags were retrieved"});
+            }
+            const [{ recomend }] = await db
+                .select({ listing_id: listings.listing_id})
+                .from(listings)
+                .where(inArray(tags.tag_name, tags),and(not(eq(user_id_string,likes.account_id))));
+            return res.status(400).json({ data: recomend});
+        }
+        else {
+            return res.status(401).json({ message: "No session cookie found. Please sign in"});
+        }
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "An unexpected error occurred", error: String(error) });
+    }
+});
+
 async function getMatches(account_id: string, owner_id: string) {
   return await db
     .select({ listing_id: likes.listing_id })
     .from(likes)
     .where(and(eq(likes.like, true), and(eq(likes.account_id, owner_id), eq(likes.listing_owner_id, account_id))));
+}
+
+//Retrieves the tags for the posts the user has liked
+async function getTags(account_id){
+    return await db
+        .select({ tags_name: tags.tag_name})
+        .from(tags)
+        .where(eq(account_id, likes.account_id),and(eq(likes.like, true)),and(eq(listing.id, tags.listing_id)));
 }
