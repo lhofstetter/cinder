cf46b879b32aa2454f9d5f8fb75b8fe98510029b
Luke Hofstetter <ldhofstetter@gmail.com>
2023-12-15 14:10:19 -0800

    [frontend] added range slider for tops

--- components/Explore.js
+++ components/Explore.js
@@ -2,16 +2,17 @@ import React, { useState, useEffect } from "react";
 import { ImageBackground, Text, View, Pressable, useWindowDimensions, Platform, ActivityIndicator, Alert, Image, TextInput } from "react-native";
 import { Svg, Path } from 'react-native-svg';
 import Dialog, { DialogContent } from 'react-native-popup-dialog';
+import MultiSlider from '@ptomasroos/react-native-multi-slider';
 import TinderCard from "react-tinder-card";
 import * as ImagePicker from "expo-image-picker";
 import { exploreStyles } from "../styles";
 import * as SecureStore from "expo-secure-store";
 import { useNavigation } from "@react-navigation/native";
-import Slider from '@react-native-community/slider';
 import axios from "axios";
 
 
 const alreadyRemoved = [];
+const categories = ["Tops", "Bottoms", "Shoes", "Accessories"];
 
 const SwipeableCard = ({ character, index, swiped, outOfFrame }) => {
   const [color, setColor] = useState([null, 1.0, "#fff", ""]);
@@ -113,8 +114,23 @@ const Advanced = () => {
   const [actualRefresh, setActualRefresh] = useState(true);
   const [settings, setSettings] = useState({visible: false});
   const [pressed, setPressed] = useState(false);
+  const [state, setState] = useState({ scrollEnabled: false});
+  const [selectedCategories, setSelectedCategories] = useState({
+    "Tops": [true, {borderStyle:"solid", borderRadius:100, borderColor:'black', borderWidth:1, display:'flex', alignItems:'center', marginLeft:"2%", backgroundColor:"#DF85FF"}],
+    "Bottoms": [true, {borderStyle:"solid", borderRadius:100, borderColor:'black', borderWidth:1, display:'flex', alignItems:'center', marginLeft:"2%", backgroundColor:"#DF85FF"}],
+    "Shoes": [true, {borderStyle:"solid", borderRadius:100, borderColor:'black', borderWidth:1, display:'flex', alignItems:'center', marginLeft:"2%", backgroundColor:"#DF85FF"}],
+    "Accessories": [true, {borderStyle:"solid", borderRadius:100, borderColor:'black', borderWidth:1, display:'flex', alignItems:'center', marginLeft:"2%", backgroundColor:"#DF85FF"}]
+  });
+  const [sizes, setSizes] = useState({
+    "Tops": {min: 1, max: 6, "numericalRange": [1, 6], "correspondingValues": ["XS", "S", "M", "L", "XL", "XXL"]},
+    "Bottoms": {min: 1, max: 7, "numericalRange": [1, 7], "correspondingValues": ["XS", "S", "M", "L", "XL", "XXL", "3XL"]},
+    "Shoes": {min: 1, max: 17, "numericalRange": [1, 17], "correspondingValues": ["6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5", "10", "10.5", "11", "11.5", "12", "12.5", "13", "13.5", "14"]},
+    "Accessories": {min: 1, max: 3, "numericalRange": [1, 3], "correspondingValues": ["S", "M", "L"]},
+  });
 
   const navigation = useNavigation();
+  const enableScroll = () => setState({ scrollEnabled: true });
+  const disableScroll = () => setState({ scrollEnabled: false });
 
   const getListings = async () => {
     try {
@@ -167,7 +183,6 @@ const Advanced = () => {
         },
       });
       const data = await response.json();
-      console.log(data);
       if (data?.matches) {
         const {data: listingData} = await axios.get(`https://cinder-server2.fly.dev/listing/${id}`);
         Alert.alert(`You matched with the owner of "${listingData.listing_name}"! Shoot them a message and coordinate a swap!"`)
@@ -198,6 +213,30 @@ const Advanced = () => {
     setCharacters(temp);
   };
 
+  function CategoryButton ({ label }) {
+    const [style, setStyle] = useState(selectedCategories[label][1]);
+
+    return (
+      <View style={style}>
+        <Pressable onPress={() => {
+          if (!selectedCategories[label][0]) {
+            selectedCategories[label][0] = !selectedCategories[label][0];
+            selectedCategories[label][1] = {borderStyle:"solid", borderRadius:100, borderColor:'black', borderWidth:1, display:'flex', alignItems:'center', marginLeft:"2%", backgroundColor:"#DF85FF"};
+            setSelectedCategories(selectedCategories);
+            setStyle(selectedCategories[label][1]);
+          } else {
+            selectedCategories[label][0] = !selectedCategories[label][0];
+            selectedCategories[label][1] = {borderStyle:"solid", borderRadius:100, borderColor:'black', borderWidth:1, display:'flex', alignItems:'center', marginLeft:"2%", backgroundColor:undefined};
+            setSelectedCategories(selectedCategories);
+            setStyle(selectedCategories[label][1]);
+          }
+        }} style={{marginLeft:"2%"}}>
+            <Text style={{fontFamily: 'Inter', padding:"2%"}}>{label}</Text>
+        </Pressable>
+      </View>
+    );
+  }
+
   return (
     <View style={exploreStyles.container}>
       <View style={{display:'flex', flexDirection:'row'}}>
@@ -220,14 +259,19 @@ const Advanced = () => {
             setSettings({ visible: false });
           }}
         >
-          <DialogContent>
-            
+          <DialogContent style={{paddingBottom:"4%"}}>
+            <Text style={{fontFamily: 'Inter', marginTop:"5%"}}>Categories</Text>
+            <View style={{display:'flex', flexDirection:'row', marginTop:"7%"}}>
+              {categories.map((category, index) => (
+                <CategoryButton label={category} key={index}/>
+              ))}
+            </View>
             <View style={{display:'flex', flexDirection:'row'}}>
               <Pressable onPress={() => {
-                setPressed(true);
-              }} style={{display: 'flex', flexDirection:"row", marginTop: '10.8%'}}>
+                setPressed(false);
+              }} style={{display: 'flex', flexDirection:"row", marginTop: '10%'}}>
 
-                { pressed ? <Svg xmlns="http://www.w3.org/2000/svg" height={30} viewBox="0 -960 960 960" width={30}>
+                { !pressed ? <Svg xmlns="http://www.w3.org/2000/svg" height={30} viewBox="0 -960 960 960" width={30}>
                       <Path
                         d="M480-280q83 0 141.5-58.5T680-480q0-83-58.5-141.5T480-680q-83 0-141.5 58.5T280-480q0 83 58.5 141.5T480-280Zm0 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
                         fill="black" 
@@ -238,12 +282,13 @@ const Advanced = () => {
                         fill="black" 
                       />
                     </Svg>}
-                <Text style={{fontFamily: 'Inter', marginLeft: '7%', marginTop: '5%'}}>One Size</Text>
+                <Text style={{fontFamily: 'Inter', marginLeft: '7%', marginTop: '4%'}}>One Size</Text>
               </Pressable>
               <Pressable onPress={() => {
-                setPressed(false);
-              }} style={{display: 'flex', flexDirection:'row', marginTop: '10%'}}>
-                { !pressed ? <Svg xmlns="http://www.w3.org/2000/svg" height={30} viewBox="0 -960 960 960" width={30}>
+                setPressed(true);
+                setSelectedCategories(selectedCategories);
+              }} style={{display: 'flex', flexDirection:'row', marginTop: '9.2%'}}>
+                { pressed ? <Svg xmlns="http://www.w3.org/2000/svg" height={30} viewBox="0 -960 960 960" width={30}>
                     <Path
                       d="M480-280q83 0 141.5-58.5T680-480q0-83-58.5-141.5T480-680q-83 0-141.5 58.5T280-480q0 83 58.5 141.5T480-280Zm0 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
                       fill="black" 
@@ -256,7 +301,35 @@ const Advanced = () => {
                     </Svg>}
                 <Text style={{fontFamily: 'Inter', marginLeft: '7%', marginTop: '5%'}}>Many Sizes</Text>
               </Pressable>
-              
+            </View>
+            <View style={{display:"flex", flexDirection:'column'}}>
+              {selectedCategories.Tops[0] && pressed ? <MultiSlider
+                      onValuesChangeStart={disableScroll}
+                      onValuesChangeFinish={enableScroll}
+                      values={sizes.Tops.numericalRange}
+                      onValuesChange={(newValues) => {
+                        sizes.Tops.numericalRange = newValues;
+                        setSizes(sizes);
+                      }}
+                      smoothSnapped={true}
+                      step={1}
+                      min={sizes.Tops.min}
+                      max={sizes.Tops.max - 1}
+                      enableLabel={true}
+                      showSteps={true}
+                      showStepsLabels={true}
+                      selectedStyle={{backgroundColor: "#DF85FF"}}
+                      customLabel={() => (
+                        <View style={{marginTop:"5%", marginLeft:"2%", display:"flex", flexDirection:"row"}}><Text style={{fontFamily: "Inter"}}>Top Sizes: {sizes.Tops.correspondingValues[sizes.Tops.numericalRange[0]]} - {sizes.Tops.correspondingValues[sizes.Tops.numericalRange[1]]}</Text></View>
+                      )}
+                      stepsAs={[{
+                        index:0,
+                        stepLabel: "XS",
+                      }, {
+                        index:sizes.Tops.max - 1,
+                        stepLabel: "XXL",
+                      }]}
+                      />: <></>}
             </View>
           </DialogContent>
         </Dialog>
